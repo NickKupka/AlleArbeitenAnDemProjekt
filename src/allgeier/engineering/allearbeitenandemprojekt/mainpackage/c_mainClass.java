a4d129ffd1ab651fb831cd262b6ae0b2f5a8735c
Nick Kupka <nick.kupka@gmail.com>
2018-01-10 16:24:38 +0100

    added a private static integer variable for the fibo method

--- src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
+++ src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
@@ -2,6 +2,7 @@ package allgeier.engineering.allearbeitenandemprojekt.mainpackage;
 
 public class c_mainClass {
 
+	private static int i_fiboZahl = 4;
 	
 	public static void call_FiboPrintOut(){
 		printFibo();
