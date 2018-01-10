86c5d9dde53dc9dc678e9c387fc923a31f998856
Nick Kupka <nick.kupka@gmail.com>
2018-01-10 16:22:04 +0100

    nick commit call printFibo()

--- src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
+++ src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
@@ -2,7 +2,13 @@ package allgeier.engineering.allearbeitenandemprojekt.mainpackage;
 
 public class c_mainClass {
 
+	
+	public static void call_FiboPrintOut(){
+		printFibo();
+	}
+	
 	public static void main(String[] args) {
+		c_mainClass.call_FiboPrintOut();
 	}
 
 }
