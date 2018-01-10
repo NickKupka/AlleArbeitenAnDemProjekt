cf6a2ebf27765250a10b3db87571b9038a8cefb2
armin.donnerstag <nck.kupka@gmail.com>
2018-01-10 16:51:13 +0100

    changed fibo value from 9 to 4 (reverted from commit f296d13e13f93188d3f1cd391007b40ff780fd26)

--- src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
+++ src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
@@ -1,27 +1,29 @@
 package allgeier.engineering.allearbeitenandemprojekt.mainpackage;
 
+
 public class c_mainClass {
 
-	private static int i_fiboValue = 4;
-
-	public static void call_FiboPrintOut() {
+	private static int i_fiboValue = 9;
+	
+	public static void call_FiboPrintOut(){
 		printFibo();
 	}
-
+	
 	public static void main(String[] args) {
 		c_mainClass.call_FiboPrintOut();
 	}
 
 	public static int fibo(int i) {
-		if (i <= 1)
-			return i;
-		else
-			return fibo(i - 1) + fibo(i - 2);
+        if (i <= 1) return i;
+        else return fibo(i-1) + fibo(i-2);
+    }
+		
 	}
 
-	public static void printFibo() {
+	public static void printFibo(){
 		System.out.println("Fibo of: " + i_fiboValue + " = " + fibo(i_fiboValue));
-
+		
 	}
-
+	
 }
+
