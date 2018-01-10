eb98006419fc0fa5d70e11296bf7a9055c8bffd0
Nick Kupka <nick.kupka@gmail.com>
2018-01-10 16:12:30 +0100

    second commit where I removed the lines in c_mainClass in developer branch

--- src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
+++ src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
@@ -3,8 +3,6 @@ package allgeier.engineering.allearbeitenandemprojekt.mainpackage;
 public class c_mainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
