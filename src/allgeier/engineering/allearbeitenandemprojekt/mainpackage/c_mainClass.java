3565a90770e735e87ae8cf956c85a89525612a19
Nick Kupka <nick.kupka@gmail.com>
2018-01-10 16:11:09 +0100

    Initial commit for the project

---
+++ src/allgeier/engineering/allearbeitenandemprojekt/mainpackage/c_mainClass.java
@@ -0,0 +1,10 @@
+package allgeier.engineering.allearbeitenandemprojekt.mainpackage;
+
+public class c_mainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
